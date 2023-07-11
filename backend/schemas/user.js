5e8fddb2fb35d98dc231b5ea7046f05152654172
Riley <akpotohwoo@gmail.com>
2023-07-11 22:08:38 +0100

    Initial commit

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            type: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            type: 'string'
+        }
+    ]
+}
